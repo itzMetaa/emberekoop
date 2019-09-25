57ff3d95da88cb63cd75235ec4ab8081eda21724
Diak <Diak@10.4.117.15>
2019-09-25 14:23:11 +0200

    ember alap

---
+++ src/ember/Ember.java
@@ -0,0 +1,16 @@
+package ember;
+
+public class Ember {
+
+    private String nev;
+    private String szulHely;
+    private String szulIdo;
+    
+    public Ember(String nev, String szulHely, String szulIdo) {
+        this.nev = nev;
+        this.szulHely = szulHely;
+        this.szulIdo = szulIdo;
+    }
+        
+        
+}
